bd282b02c95872bc2c194cc1b06d801acff38c0b
derdilla <contact@derdilla.com>
2025-03-27 20:24:31 +0100

    Make graph interactive

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from typing import List
 
 import altair as alt
 import pandas as pd
+import numpy as np
+import os
 
 
 def get_table(file):
@@ -40,6 +42,14 @@ def split_frames(df):
 
     # remove turning columns
     df = df[df['dir'] != 'turning']
+
+    # insert nan where distance gap is to big
+    pd.options.mode.copy_on_write = True
+    df['Distance (GPS)'] = pd.to_numeric(df['Distance (GPS)'])
+    df['Stroke Rate'] = pd.to_numeric(df['Stroke Rate'])
+    df.loc[df['Distance (GPS)'].diff() > 100, ['Distance (GPS)', 'Stroke Rate']] = np.nan
+    df.loc[df['Stroke Rate'] < 10 , ['Distance (GPS)', 'Stroke Rate']] = np.nan
+    df.loc[df['Stroke Rate'] > 34 , ['Distance (GPS)', 'Stroke Rate']] = np.nan
     return df
 
 def create_joined_dataset(file_names: List[str]) -> pd.DataFrame:
@@ -55,30 +65,52 @@ def create_joined_dataset(file_names: List[str]) -> pd.DataFrame:
 
 def draw(data):
     alt.renderers.enable("html")
+
     # draw 2 lines allow toggling depending on 'dir' parameter and limit the size
-    chart = alt.Chart(data).mark_line().encode(
-        x=alt.X('Distance (GPS)', title='Distance (m)', axis=alt.Axis(
-            values=range(0, 1000, 100),
-            labelAngle=0  # Keep labels horizontal (optional)
-        )),
-        y=alt.Y('Stroke Rate', title='Stroke Rate (spm)'), # alt.Y('Split (GPS)', title='Split (GPS)'),
+    multi = alt.selection_point(fields=['file_name'])
+    chart = alt.Chart(data).mark_line(
+        strokeWidth=2
+    ).add_params(
+        multi
+    ).encode(
+        x=alt.X('Distance (GPS)', title='Distance (m)', scale=alt.Scale(domain=[0, 14000]), axis=alt.Axis(labelAngle=-45)),
+        y=alt.Y('Stroke Rate', title='Stroke Rate (spm)', scale=alt.Scale(domain=[10, 35])), # alt.Y('Split (GPS)', title='Split (GPS)'),
         tooltip=alt.Tooltip('Stroke Rate', title='Stroke Rate (spm)'),
-        # set the color based on the 'file_name' column
-        color='file_name:N',
+        # set the color based on the 'file_name' column, allowing selection
+        color=alt.condition(multi, 'file_name:N', alt.value('lightgray')),
+        detail='group:N'
     ).properties(
-        width=800,
-        height=400,
+        width=1200,
+        height=250,
     ).facet(
-        column='dir:N'  # Creates separate charts for 'up' and 'down'
+        row='dir:N'  # Creates separate charts for 'up' and 'down'
     )
 
-    chart.save('index.html', embed_options={'renderer':'svg'})
+    length = alt.Chart(data).mark_bar(size=30).encode(
+        y=alt.Y('file_name:N', title='File Name', sort='-y'),  # Sort by distance
+        x=alt.X('max(Distance (GPS)):Q', title='Distance (m)', scale=alt.Scale(zero=True)),  
+        color=alt.Color('file_name:N', legend=None, scale=alt.Scale(scheme='tableau20')),  
+        tooltip=['file_name:N', 'max(Distance (GPS)):Q']  # Add tooltips
+    ).add_params(
+        multi
+    ).properties(
+        width=1200,
+        height=400,
+        title="Max GPS Distance by File"
+    )
+
+    (chart & length).save('index.html', embed_options={'renderer':'svg'})
 
  
 def main():
     # Interval,     Distance (GPS),     Distance (IMP),     Elapsed Time,Split (GPS),Speed (GPS),Split (IMP),Speed (IMP),Stroke Rate,Total Strokes,Distance/Stroke (GPS),Distance/Stroke (IMP),Heart Rate,Power,Catch,Slip,Finish,Wash,Force Avg,Work,Force Max,Max Force Angle,GPS Lat.,GPS Lon.
     # (Interval),   (Meters),           (Meters),           (HH:MM:SS.tenths),(/500),(M/S),(/500),(M/S),(SPM),(Strokes),(Meters),(Meters),(BPM),(Watts),(Degrees),(Degrees),(Degrees),(Degrees),(Newtons),(Joules),(Newtons),(Degrees),(Degrees),(Degrees)
-    data = create_joined_dataset(['csv-samples/SpdCoach 3039416 20250307 1205PM.csv', 'csv-samples/SpdCoach 3039416 20250308 0240PM.csv'])
+    
+    # get a list of all files in csv-samples
+    files = ['csv-samples/' + f for f in os.listdir('csv-samples') if f.endswith('.csv')]
+
+    # create a dataset of all files in csv-samples, by indexing the dir
+    data = create_joined_dataset(files)
     draw(data)
 
  
